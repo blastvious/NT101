2009307af83aeefded5514d07696b7d15bb3bfd5
blastvious <silverhd280105@gmail.com>
2025-12-09 16:45:10 +0700

    update code

---
+++ Week6/AES/GUI/GUI_APP.py
@@ -0,0 +1,18 @@
+##################
+# MSSV 23521717
+# Nguyen Anh Tuan
+# GUI_APP.py
+# git https://github.com/blastvious/NT101
+##################
+
+from tkinter import*
+
+TitleName = "Name Program here !"
+
+root = Tk()
+
+root.title(TitleName)
+
+root.geometry('800x400')
+
+root.mainloop()
---
+++ Week6/GUI/GUI_APP.py
@@ -0,0 +1,11 @@
+from tkinter import*
+
+TitleName = "Name Program here !"
+
+root = Tk()
+
+root.title(TitleName)
+
+root.geometry('800x400')
+
+root.mainloop()
